83966e5525af51f95842f065d2eac25eb3539b7c
Paul van der Slot <paul.vd.slot@rigd-loxia.nl>
2019-10-02 11:45:58 +0200

    Refactor update for every item

--- Java/src/main/java/com/gildedrose/GildedRose.java
+++ Java/src/main/java/com/gildedrose/GildedRose.java
@@ -17,46 +17,47 @@ class GildedRose {
         if (item.name.equals(SpecialItemNames.AGED_BRIE)) {
             updateAgedBrie(item);
         }
+        else if (item.name.equals(SpecialItemNames.SULFURAS)) {
+            // do nothing
+        }
+        else if (item.name.equals(SpecialItemNames.BACKSTAGE_PASSES)) {
+            updateBackstagePasses(item);
+        }
         else {
+            updateBasicItem(item);
+        }
+    }
 
-            if (!item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                if (item.quality > 0 && !item.name.equals("Sulfuras, Hand of Ragnaros")) {
-                    decreaseQuality(item);
-                }
-            }
-            else {
-                if (item.quality < 50) {
-                    increaseQuality(item);
-                    if (item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                        if (item.sellIn < 11 && item.quality < 50) {
-                            increaseQuality(item);
-                        }
-                        if (item.sellIn < 6 && item.quality < 50) {
-                            increaseQuality(item);
-                        }
-                    }
-                }
-            }
+    private void updateBasicItem(Item item) {
+        decreaseSellIn(item);
 
-            if (!item.name.equals("Sulfuras, Hand of Ragnaros")) {
-                item.sellIn = item.sellIn - 1;
-            }
+        decreaseQuality(item);
+        if (item.sellIn < 0) {
+            decreaseQuality(item);
+        }
+    }
 
-            if (item.sellIn < 0) {
-                if (!item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                    if (item.quality > 0 && !item.name.equals("Sulfuras, Hand of Ragnaros")) {
-                        decreaseQuality(item);
-                    }
-                }
-                else {
-                    item.quality = 0;
-                }
-            }
+    private void decreaseSellIn(Item item) {
+        item.sellIn = item.sellIn - 1;
+    }
+
+    private void updateBackstagePasses(Item item) {
+        decreaseSellIn(item);
+
+        increaseQuality(item);
+        if (item.sellIn < 11) {
+            increaseQuality(item);
+        }
+        if (item.sellIn < 6) {
+            increaseQuality(item);
+        }
+        if (item.sellIn < 0) {
+            item.quality = 0;
         }
     }
 
     private void updateAgedBrie(Item item) {
-        item.sellIn = item.sellIn - 1;
+        decreaseSellIn(item);
 
         increaseQuality(item);
         if (item.sellIn < 0) {
@@ -71,6 +72,8 @@ class GildedRose {
     }
 
     private void decreaseQuality(Item item) {
-        item.quality = item.quality - 1;
+        if (item.quality > 0) {
+            item.quality = item.quality - 1;
+        }
     }
 }
